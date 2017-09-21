e923045ba8652d66fefdee7c1800ee4b2756a497
bpa <bpa@ugraweb.ru>
2017-09-21 13:13:07 +0500

    Добавлен адаптив для всех устройств. Протестировано в браузерах Chrome, Mozilla, Edge и IE11

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -2,28 +2,50 @@
   $(function () {
 
     // плавный переход по якорям
-    $('.js-scroll').on('click', (e) => {
-      e.preventDefault()
-      $('html, body').animate({ scrollTop: $(e.currentTarget.hash).offset().top })
-    })
+    {
+      $('.js-scroll').on('click', (e) => {
+        e.preventDefault()
+        $('html, body').animate({scrollTop: $(e.currentTarget.hash).offset().top})
+      })
+    }
 
     // появление при скролле страницы значка НАВЕРХ
-    const scrollButton = $('.js-arrow')
-    const pixels = 200
-    const $window = $(window)
-    const animateButton = (() => {
-      return (opacityValue) => {
-        scrollButton.stop().animate({ opacity: opacityValue }, 200)
-      }
-    })()
-    $window.scroll((e) => { animateButton(+(e.currentTarget.pageYOffset > pixels)) });
-    // если перезагрузили страницу, но скролл ниже 200 пикселей, то показываем значек сразу
-    if ($window.scrollTop() > pixels) { animateButton(1) }
-    // событие на клик по значку НАВЕРХ
-    scrollButton.on('click', (e) => {
-      e.preventDefault()
-      $('html, body').animate({ scrollTop: 0 })
-    })
+    {
+      const scrollButton = $('.js-arrow')
+      const pixels = 200
+      const $window = $(window)
+      const animateButton = (() => {
+        return (opacityValue) => {
+          scrollButton.stop().animate({opacity: opacityValue}, 200)
+        }
+      })()
+      $window.scroll((e) => { animateButton(+(e.currentTarget.pageYOffset > pixels)) })
+      // если перезагрузили страницу, но скролл ниже 200 пикселей, то показываем значек сразу
+      if ($window.scrollTop() > pixels) { animateButton(1) }
+      // событие на клик по значку НАВЕРХ
+      scrollButton.on('click', (e) => {
+        e.preventDefault()
+        $('html, body').animate({scrollTop: 0})
+      })
+    }
+
+    // инициализация слайдера
+    {
+      $('.swiper-container').swiper({
+        slidesPerView: 'auto'
+      })
+    }
+
+    // мобильное меню
+    {
+      $('#menu-icon').on('click', (e) => {
+        e.preventDefault()
+        const $this = $(e.currentTarget)
+        const $mobileMenu = $($this.val())
+        $this.toggleClass('open')
+        $mobileMenu.slideToggle()
+      })
+    }
 
   })
 })(jQuery)
